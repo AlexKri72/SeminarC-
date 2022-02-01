93e1a9a08d5748de5d4c7bd694420d3ce2230823
AlexKri72 <krial11@rambler.ru>
2022-02-01 20:38:15 +0500

    task008

---
+++ task008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Clear();
+int n = new Random().Next(1,101);
+int index = 1;
+Console.WriteLine("Задано максимальное число: " + n);
+Console.Write("Четные числа : ");
+while (index <= n )
+{
+    if ( index % 2 == 0 )
+        {
+            Console.Write( index + " ");
+        }
+    index++;
+}
+
