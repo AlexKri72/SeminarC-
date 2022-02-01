1a60e7c09c429c63a908524c3acd030871f40df7
AlexKri72 <krial11@rambler.ru>
2022-02-01 20:37:15 +0500

    task006

---
+++ task006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, является ли число чётным
+
+Console.Clear();
+int n = new Random().Next(1, 101);
+Console.WriteLine("Подобрано число: " + n);
+if (n % 2 == 0)
+{
+    Console.WriteLine("Это чётное число");
+}
+else
+{
+    Console.WriteLine("Это нечетное число");
+}
