b732748a5d3481798af5d6143b6017198e9b8fca
AlexKri72 <krial11@rambler.ru>
2022-02-01 20:37:44 +0500

    task007

---
+++ task007/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int count = n;
+n = n - 2 * n;
+while (n <= count)
+{
+    Console.Write(n + " ");
+    n++;
+}
