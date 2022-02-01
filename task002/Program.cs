421125c6fde930080c9c0e785c3332a0a52d40c0
AlexKri72 <krial11@rambler.ru>
2022-02-01 20:35:35 +0500

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Даны два числа. Показать большее и меньшее число
+Console.Clear();
+Console.Write("Введите первое число: ");
+int n1 = int.Parse (Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = int.Parse (Console.ReadLine());
+int max,min;
+if (n1>n2)
+{
+    max = n1;
+    min = n2;
+}
+else
+{
+    max = n2;
+    min = n1;
+}
+Console.WriteLine("Max = " + max + ", Min = " + min);
