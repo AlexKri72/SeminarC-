11e26606e730978f1d0efce4cb9bdeddd50d9004
AlexKri72 <krial11@rambler.ru>
2022-02-04 11:10:38 +0500

    task 010

---
+++ task010/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Clear();
+
+string text = Convert.ToString(new Random().Next(100,1000));
+Console.WriteLine($"Введено случайное трехзначное число : {text}"); 
+Console.WriteLine($"Вторая цифра равна {text[1]} ");
+
+Console.WriteLine();
