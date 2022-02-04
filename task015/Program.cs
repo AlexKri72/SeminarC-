451d719a8de43f69da3c44746eb29a0b0f53ca51
AlexKri72 <krial11@rambler.ru>
2022-02-04 11:14:28 +0500

    task 015

---
+++ task015/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse (Console.ReadLine());
+if (a % 7 ==0 && a % 23 ==0) Console.Write("Введенное число кратно 7 и 23 ");
+else Console.Write("Введенное число не кратно 7 и 23");
+
