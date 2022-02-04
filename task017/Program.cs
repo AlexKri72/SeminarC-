67366af25fe73f9ffbbc1edfa4266037ff233aa1
AlexKri72 <krial11@rambler.ru>
2022-02-04 11:32:28 +0500

    task 017

---
+++ task017/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Clear();
+
+Console.Write($"Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write($"Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a == b * b || b == a * a) Console.WriteLine("Одно из чисел является квадратом другого!");
+else Console.WriteLine("Числа не являются квадратами друг друга!");
+
+Console.WriteLine();
