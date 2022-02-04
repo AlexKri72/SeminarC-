5fc8984114703939a91446b229d1080eb3185d4b
AlexKri72 <krial11@rambler.ru>
2022-02-04 11:11:27 +0500

    task 011

---
+++ task011/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Clear();
+
+int a = new Random().Next(10, 100);
+Console.WriteLine($"Задано число: {a}");
+int b = a % 10;
+int c = a / 10;
+
+if (b > c) Console.WriteLine($"Наибольшая цифра в числе : {b}");
+else Console.WriteLine($"Наибольшая цифра в числе : {c}");
+
+Console.WriteLine();
+
