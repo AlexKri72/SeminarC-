3bc17d88f621698418cbcc4b1940413adfba252d
AlexKri72 <krial11@rambler.ru>
2022-02-04 11:13:04 +0500

    task 014

---
+++ task014/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Clear();
+
+string text = Convert.ToString(new Random().Next(1, 201));
+Console.WriteLine($"Введено случайное число: {text}");
+if (text.Length >= 3)   Console.WriteLine($"Третья цифра числа: {text[2]}");
+else                    Console.WriteLine("Число слишком короткое, нет третьей цифры!");
+
+Console.WriteLine();
