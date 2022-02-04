0a68cd16470497dd458b59a398f609b9acebc556
AlexKri72 <krial11@rambler.ru>
2022-02-04 11:12:01 +0500

    task 012

---
+++ task012/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+
+string text = Convert.ToString(new Random().Next(100,1000));
+Console.WriteLine($"Введено случайное трехзначное число : {text}"); 
+text = text.Remove(1,1);
+Console.WriteLine($"Без второй цифры осталось {text} ");
+
+Console.WriteLine();
