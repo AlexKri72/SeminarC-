3afc9612bdd729631a8d8e94c4be50cea0c035b6
AlexKri72 <krial11@rambler.ru>
2022-02-08 18:50:04 +0500

    task 035

---
+++ task035/Program.cs
@@ -0,0 +1,28 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+Console.Clear();
+
+int[] array = new int[20];
+void FillArray(int[] massive)
+{
+    for (int i = 0; i < massive.Length; i++)
+        massive[i] = new Random().Next(-500, 501);
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} \t ");
+}
+// закончились методы, началось тело программы
+
+FillArray(array);
+PrintArray(array);
+Console.Write("\nКакое число ищем? : ");
+int a = int.Parse(Console.ReadLine());
+
+int b = Array.IndexOf(array, a);
+if (b == -1) Console.WriteLine("Данное число отсутствует в массиве!");
+else Console.WriteLine($"Данный элемент находится на {b + 1} позиции массива");
+
+Console.WriteLine();
+Console.WriteLine();
