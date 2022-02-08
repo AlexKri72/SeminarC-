4e3b37c34e07581220d68e1c511ee0b5e2d11ade
AlexKri72 <krial11@rambler.ru>
2022-02-08 11:12:51 +0500

    task 025

---
+++ task025/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Clear();
+
+int sum = 0;
+int a = new Random().Next(1, 25);
+for (int i = 1; i <= a; i++) sum += i;
+
+Console.WriteLine($"Задано случайное число : {a}");
+Console.WriteLine($"Сумма от 1 до {a} равна: {sum}");
+
+Console.WriteLine();
