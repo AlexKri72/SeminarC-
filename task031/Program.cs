906fa2e9723d845cfca5ccf04b133dc4449b8749
AlexKri72 <krial11@rambler.ru>
2022-02-08 12:37:30 +0500

    task 031

---
+++ task031/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+Console.Clear();
+
+int[] array = new int[8] {34,56,2,72,8,56,7,16};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+}
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine();
