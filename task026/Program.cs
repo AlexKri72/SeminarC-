a8266670c7f05d37198496b3495c80981bf3f4b7
AlexKri72 <krial11@rambler.ru>
2022-02-08 11:27:10 +0500

    task 026

---
+++ task026/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("В какую степень будем возводить? : ");
+int b = int.Parse(Console.ReadLine());
+int degree = a;
+for (int i = 2; i <= b ; i++) degree *= a;
+
+Console.WriteLine($"{a} в степени {b} равно: {degree}");
+
+Console.WriteLine();
