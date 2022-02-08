aa9bc327f3854d4abda6594e1d9e9cbb19fd4d8c
AlexKri72 <krial11@rambler.ru>
2022-02-08 18:29:43 +0500

    task 034

---
+++ task034/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+Console.Clear();
+
+int[] array = new int[12];
+void FillArray(int[] massive)
+{
+    for (int i = 0; i < massive.Length; i++)
+        massive[i] = new Random().Next(-9, 10);
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} \t ");
+}
+FillArray(array);
+Console.Write($"Первоначально заданный массив чисел: \n");
+PrintArray(array);
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) array[i] -= array[i] * 2;
+    else array[i] -= array[i] * 2;
+}
+
+Console.Write($"\nИвертированный массив чисел: \n");
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine();
