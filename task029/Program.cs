1894d940fd413fec5a7ef0b2d73abf7747ae2613
AlexKri72 <krial11@rambler.ru>
2022-02-08 11:57:20 +0500

    task 029

---
+++ task029/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+int composition = 1;
+int a = new Random().Next(1, 20);
+for (int i = 1; i <= a; i++) composition *= i;
+
+Console.WriteLine($"Задано случайное число : {a}");
+Console.WriteLine($"Произведение цифр от 1 до {a} равно: {composition}");
+
+Console.WriteLine();
