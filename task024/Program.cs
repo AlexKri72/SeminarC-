efcd709008da3aa0fed4a57b07939c38230757c1
AlexKri72 <krial11@rambler.ru>
2022-02-08 11:11:21 +0500

    task 024

---
+++ task024/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Clear();
+
+int a = new Random().Next(1, 25);
+Console.WriteLine("\t Число  \t Куб числа");
+for (int i = 1; i <= a; i++) Console.WriteLine($"\t {i}  \t\t {(i * i * i)}");
+
+Console.WriteLine();
