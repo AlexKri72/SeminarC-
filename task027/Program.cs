c850643d8ce9f52c74e85654c188c74eab337769
AlexKri72 <krial11@rambler.ru>
2022-02-08 11:34:39 +0500

    task 027

---
+++ task027/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+
+Console.Clear();
+
+Console.Write("Задайте любое целое число: ");
+int a = int.Parse(Console.ReadLine());
+string b = Convert.ToString(a);
+Console.WriteLine($"Количество цифр в заданном числе равно: {b.Length}");
+
+Console.WriteLine();
