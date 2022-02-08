df76816452536835343b8694b38584de15a36e88
AlexKri72 <krial11@rambler.ru>
2022-02-08 10:48:52 +0500

    task 023

---
+++ task023/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+
+int a = new Random().Next(1, 25);
+Console.WriteLine("\t Число  \t Квадрат числа");
+for (int i = 1; i <= a; i++) Console.WriteLine($"\t {i}  \t\t {(i * i)}");
+
+Console.WriteLine();
