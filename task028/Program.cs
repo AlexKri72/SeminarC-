fc64d8ebe33ca8e0b75c698aac82d2d5b02c18a7
AlexKri72 <krial11@rambler.ru>
2022-02-08 11:48:02 +0500

    task 028

---
+++ task028/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+
+int a = new Random().Next(11,1000000);
+Console.WriteLine($"Задано случайное число: {a}");
+int sum = 0;
+while (a > 0)
+{
+    sum += a % 10;
+    a = a / 10;
+}
+Console.WriteLine($"Сумма цифр в заданном числе равна: {sum}");
+
+Console.WriteLine();
