68f210e64c5022c942707f8848abf2bc84ffb67b
AlexKri72 <krial11@rambler.ru>
2022-02-08 12:26:46 +0500

    task 030

---
+++ task030/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+
+Console.Write("Введите максимальную цифру диапазона от 1 до : ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("\t Число \t \t Куб числа, заканчивающегося на чётную цифру");
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0 && i % 10 != 0) Console.WriteLine($"\t {i} \t \t {i * i * i}");
+
+Console.WriteLine();
