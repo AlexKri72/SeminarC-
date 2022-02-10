a2e8e30a53ee5237253eb7f99906434b12f63128
AlexKri72 <krial11@rambler.ru>
2022-02-10 22:14:36 +0500

    task 018

--- task018/Program.cs
+++ task018/Program.cs
@@ -5,7 +5,7 @@ Console.Clear();
 
 bool[] x = { true, false };
 bool[] y = { true, false };
-bool z,z1;
+bool z, z1;
 Console.WriteLine("          X              Y            ¬(X v Y)         ¬X ^ ¬Y            ¬(X v Y) = ¬X ^ ¬Y      ");
 Console.WriteLine("________________________________________________________________________________________________");
 int index = 0;
@@ -14,12 +14,12 @@ while (index <= 1)
     int count = 0;
     while (count <= 1)
     {
-        z = !(x[index] || y[count]);
-        Console.Write($"     \t{x[index]}       \t{y[count]}      \t{z}");
-        z1 = !x[index] && !y[count];
-        Console.Write($"         \t{z1}");
-        if (z==z1) Console.WriteLine($"                \t{z==z1}");
-        else Console.WriteLine($"         \t{z==z1}");
+        z = !(x[index] || y[count]); // левая часть равенства
+        Console.Write($"\t{x[index]} \t\t{y[count]} \t\t{z}");
+        z1 = !x[index] && !y[count];  // правая часть равенства
+        Console.Write($"\t\t{z1}");
+        if (z == z1) Console.WriteLine($"\t\t\t{z == z1}");
+        else Console.WriteLine($"\t\t\t{z == z1}");
         count++;
     }
     index++;
