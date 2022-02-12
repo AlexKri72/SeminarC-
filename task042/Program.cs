a30a2c25afddd6e098ed08fb0c105848c39c958d
AlexKri72 <krial11@rambler.ru>
2022-02-12 21:34:03 +0500

    task 042

---
+++ task042/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Clear();
+
+Console.Write("Сколько чисел будем вводить? :");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n]; // задаем размер массива
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"Введите {i + 1} число: ");
+    arr[i] = int.Parse(Console.ReadLine());
+    if (arr[i] > 0) count++; // считаем положительные числа
+}
+
+Console.WriteLine($"Введено {count} чисел больше нуля.");
+
+Console.WriteLine();
