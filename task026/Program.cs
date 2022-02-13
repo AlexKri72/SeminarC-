e2e330c295c0301e547fbcc2742216eaae67359c
AlexKri72 <krial11@rambler.ru>
2022-02-13 11:51:45 +0500

    task 026

--- task026/Program.cs
+++ task026/Program.cs
@@ -6,8 +6,9 @@ Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("В какую степень будем возводить? : ");
 int b = int.Parse(Console.ReadLine());
+
 int degree = a;
-for (int i = 2; i <= b ; i++) degree *= a;
+for (int i = 1; i < b ; i++) degree *= a;
 
 Console.WriteLine($"{a} в степени {b} равно: {degree}");
 
