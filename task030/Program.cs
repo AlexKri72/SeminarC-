13162acb43ab5e26725e21e9945fbbd5d5e8772c
AlexKri72 <krial11@rambler.ru>
2022-02-14 22:09:15 +0500

    task 030 ready

--- task030/Program.cs
+++ task030/Program.cs
@@ -8,7 +8,7 @@ Console.Clear();
 Console.Write("Введите максимальную цифру диапазона от 1 до : ");
 int n = int.Parse(Console.ReadLine());
 Console.WriteLine("\t Число \t \t Куб числа, заканчивающегося на чётную цифру");
-for (int i = 1; i <= n; i++)
-    if (i % 2 == 0) Console.WriteLine($"\t {i} \t \t {i * i * i}");
+for (int i = 2; i <= n; i+=2)
+    Console.WriteLine($"\t {i} \t \t {i * i * i}");
 
 Console.WriteLine();
