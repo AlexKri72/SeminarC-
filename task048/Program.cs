6885d4c809b38750b8a6d1243a1733823afdead4
AlexKri72 <krial11@rambler.ru>
2022-02-14 19:01:51 +0500

    task 048

---
+++ task048/Program.cs
@@ -0,0 +1,35 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+Console.Clear();
+
+Console.Write("Задайте количество строк в массиве: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Задайте количество столбцов в массиве: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n]; 
+void FillArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,100);
+        }
+    }
+}
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
