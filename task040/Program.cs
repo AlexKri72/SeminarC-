af4a72412d98024a6a98acbbdb5c99d37b1fa84e
AlexKri72 <krial11@rambler.ru>
2022-02-17 12:28:13 +0500

    task 040 ready

--- task040/Program.cs
+++ task040/Program.cs
@@ -2,7 +2,7 @@
 
 Console.Clear();
 
-double[] array = new double[10];
+double[] array = new double[5];
 
 void FillArray(double[] massive)
 {
@@ -13,7 +13,7 @@ void PrintArray(double[] massive)
 {
     Console.WriteLine("\t Номер позиции \tЭлемент массива");
     for (int i = 0; i < massive.Length; i++)
-        Console.WriteLine($"\t \t{i + 1} \t \t{massive[i]:0.0000}");
+        Console.WriteLine($"\t\t{i + 1} \t   {massive[i]:0.0000}");
 }
 double FindMin(double[] arr)
 {
@@ -37,9 +37,8 @@ double FindMax(double[] arr)
 FillArray(array);
 PrintArray(array);
 Console.WriteLine($"Максимальный элемент массива равен : \t{FindMax(array):0.0000} \nМинимальный элемент массива равен : \t{FindMin(array):0.0000} ");
-Console.WriteLine($"Разница между ними равна : \t\t{FindMax(array) - FindMin(array):0.0000}");
+Console.WriteLine($"Разница между ними равна : \t\t{FindMax(array) - FindMin(array):0.0000}\n");
 
-Console.WriteLine();
 
 
 
