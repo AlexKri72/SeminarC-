e1865326f1dd9b0c6696cb39a270b6b67da8b8f0
AlexKri72 <krial11@rambler.ru>
2022-02-21 19:31:31 +0500

    task 068

---
+++ task068/Program.cs
@@ -0,0 +1,26 @@
+﻿// Рекурсия: Показать натуральные числа от M до N, N и M заданы
+
+Console.Clear();
+Console.Write("Задайте число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Задайте число n: ");
+int n = int.Parse(Console.ReadLine());
+if (m > n)
+{
+    int temp = n;
+    n = m;
+    m = temp;
+}
+
+int NaturalNumber(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write($"{m:d2} ");
+        m++;
+        NaturalNumber(m, n);
+    }
+    return m;
+}
+NaturalNumber(m, n);
+
