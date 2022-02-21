748f88ae2c12a5a7b1c6db903dcb111539b5a560
AlexKri72 <krial11@rambler.ru>
2022-02-21 18:51:04 +0500

    task 067

---
+++ task067/Program.cs
@@ -0,0 +1,19 @@
+﻿// Рекурсия: Показать натуральные числа от N до 1, N задано
+
+Console.Clear();
+
+Console.Write("Задайте число: ");
+int n = int.Parse(Console.ReadLine());
+
+int NaturalNumber(int num)
+{
+    num--;
+    if (num != 0)
+    {
+        Console.Write($"{num:d2} ");
+        NaturalNumber(num);
+    }
+    return num;
+}
+NaturalNumber(n + 1);
+
