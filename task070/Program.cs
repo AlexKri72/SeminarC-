2d31494687cd54bf8159c3910f24b5def1b95536
AlexKri72 <krial11@rambler.ru>
2022-02-22 23:06:40 +0500

    task 070

---
+++ task070/Program.cs
@@ -0,0 +1,21 @@
+﻿// Рекурсия: Найти сумму цифр числа
+
+Console.Clear();
+
+Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+int Counter(int n)
+{
+    if (n % 10 > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+        Counter(n);
+    }
+    return n;
+}
+Counter(n);
+Console.WriteLine("Сумма цифр заданного числа равна: " + sum);
+
+Console.WriteLine();
