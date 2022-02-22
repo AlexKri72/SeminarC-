b42438d93088122dcd0dacc40c0e7c77a062019d
AlexKri72 <krial11@rambler.ru>
2022-02-22 23:23:17 +0500

    task 071

---
+++ task071/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычисления функции Аккермана
+
+Console.Clear();
+
+Console.Write("Задайте число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Задайте число n: ");
+int n = int.Parse(Console.ReadLine());
+
+int Ackerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Ackerman(n - 1, 1);
+    else
+      return Ackerman(n - 1, Ackerman(n, m - 1));
+}
+Console.WriteLine($"Функция Аккермана от введенных чисел равна: {Ackerman(n,m)}\n");
