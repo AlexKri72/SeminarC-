b63e9f8d7d8c09ef92d35e3d4b2f47afeffc0e5a
AlexKri72 <krial11@rambler.ru>
2022-02-22 22:53:36 +0500

    task 069

---
+++ task069/Program.cs
@@ -0,0 +1,31 @@
+﻿// Рекурсия: Найти сумму элементов от M до N, N и M заданы
+
+Console.Clear();
+
+Console.Write("Задайте число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Задайте число n: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+if (m > n)
+{
+    int temp = n;
+    n = m;
+    m = temp;
+}
+
+int NaturalNumber(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write($"{m:d2} ");
+        sum += m;
+        m++;
+        NaturalNumber(m, n);
+    }
+    return m;
+}
+NaturalNumber(m, n);
+Console.WriteLine($"\nСумма элементов равна: {sum:n0}\n");
+
+
