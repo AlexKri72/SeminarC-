76a860d4aecec9dd712e0406c426d73f3cb0ce65
AlexKri72 <krial11@rambler.ru>
2022-02-22 18:39:50 +0500

    task 064

--- task064/Program.cs
+++ task064/Program.cs
@@ -17,11 +17,12 @@ n = int.Parse(Console.ReadLine());
 for (i = 0; i < n; i++)
 {
     for (c = 0; c <= (n - i); c++)  // создаём после каждой строки n-i отступов от левой стороны консоли,чем ниже строка, тем меньше отсутп
-        Console.Write("  ");
+        Console.Write("   ");
+        
     for (c = 0; c <= i; c++)
     {
-        int element = factorial(i) / (factorial(c) * factorial(i - c)); //получаем элемент строки треугольника
-        Console.Write("{0}",element + "    ");
+        int element = factorial(i) / (factorial(c) * factorial(i - c)); //получаем элемент строки треугольника  
+        Console.Write("{0}",element + "     ");
     }
     Console.WriteLine();
 }
