e907b33fbcc8abed1f4268fe2ae863c645ab276b
AlexKri72 <krial11@rambler.ru>
2022-02-25 16:41:48 +0500

    task 072

---
+++ task072/Program.cs
@@ -0,0 +1,17 @@
+﻿// Рекурсия: Написать программу возведения числа А в целую степень B
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("В какую степень будем возводить? : ");
+int b = int.Parse(Console.ReadLine());
+
+int Pow(int a, int pow)
+{
+    if (pow == 0) return 1;
+    if (pow > 0) return Pow(a, pow - 1) * a;
+    return 1 / Pow(a, -pow);
+}
+
+Console.WriteLine($"\n{a} в степени {b} равно: {Pow(a, b)}\n");
