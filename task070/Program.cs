fbef062ce43d8b69318c4aaa217f6471ffc1a687
AlexKri72 <krial11@rambler.ru>
2022-03-01 00:37:35 +0500

    task 070

--- task070/Program.cs
+++ task070/Program.cs
@@ -4,18 +4,12 @@ Console.Clear();
 
 Console.Write("Введите любое число: ");
 int n = int.Parse(Console.ReadLine());
-int sum = 0;
-int Counter(int n)
-{
-    if (n % 10 > 0)
-    {
-        sum += n % 10;
-        n /= 10;
-        Counter(n);
-    }
-    return n;
-}
-Counter(n);
-Console.WriteLine("Сумма цифр заданного числа равна: " + sum);
 
-Console.WriteLine();
+int Summa(int n)
+{
+    if (n > 0) return n % 10 + Summa(n / 10);
+    else return 0;
+}
+
+Console.WriteLine("Сумма цифр заданного числа равна: " + Summa(n));
+
