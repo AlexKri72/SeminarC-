595b3f2bc5df03e5aae6359fc8e1afb51a43e42b
AlexKri72 <krial11@rambler.ru>
2022-03-01 00:43:15 +0500

    task 068

--- task068/Program.cs
+++ task068/Program.cs
@@ -5,22 +5,19 @@ Console.Write("Задайте число m: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Задайте число n: ");
 int n = int.Parse(Console.ReadLine());
-if (m > n)
-{
-    int temp = n;
-    n = m;
-    m = temp;
-}
 
-int NaturalNumber(int m, int n)
+void NaturalNumber(int m, int n)
 {
-    if (m <= n)
+    if (m < n)
     {
         Console.Write($"{m:d2} ");
-        m++;
-        NaturalNumber(m, n);
+        NaturalNumber(m + 1, n);
     }
-    return m;
+    if (m > n)
+    {
+        Console.Write($"{m:d2} ");
+        NaturalNumber(m - 1, n);
+    }
 }
 NaturalNumber(m, n);
 
